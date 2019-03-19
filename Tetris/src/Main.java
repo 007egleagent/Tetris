c6b44471a42b2d1f223303b1fa0679e135d5ee48
LAPTOP-S14IBJ3C\rlpiq <rlpiquer@gmail.com>
2019-03-19 00:04:21 -0600

    number2
    made the arrays

---
+++ Tetris/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+//		int[] intArray = new int[5]
+//		int[] intArray = {1,2,3,4,5}
+
+		int[] arr = new int[5];
+		
+		arr[0] = 69;
+		arr[2] = 45;
+		arr[4] = 24;
+		
+		for (int i = 0; i < arr.length; i++)
+			System.out.println( i + arr[i]);
+		
+	}
+}
+
+
