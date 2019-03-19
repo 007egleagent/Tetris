4bda1fcd42ae56de97e33d8b820347486b74fe61
LAPTOP-S14IBJ3C\rlpiq <rlpiquer@gmail.com>
2019-03-19 12:30:26 -0600

    4th
    made it print the rows and columns values

--- Tetris/src/Main.java
+++ Tetris/src/Main.java
@@ -1,35 +1,39 @@
 public class Main {
 	
-	private static int rows;
-	private static int columns;
+	private int rows;
+	private int columns;
 	
 	
 	
-	public Main(int rows, int columns) {
-		rows = 8;
-		columns = 120;
+	public Main() {
+		this.rows = 8;
+		this.columns = 120;
 		
 	}
 	
-	public static void main(String[] args) {
-	
-	int[][] gameboard = new int[rows][columns];
-	
-//	gameboard[rows][columns] = 69;
-	
-	for(int i = 0; i < 3; i++)
-	{
-	  for(int y = 0; y < 9; y++)
-	  {
-	    int value = 0;
-//		gameboard[rows][columns] = new value[0][0];
-		System.out.println(gameboard[rows][columns]);
-	  }
-	  
-	}
-	
-}
+    public void setRows(int rows){
+        this.rows = rows;
+    }
+    
+    public void setColumns(int columns) {
+    	this.columns = columns;
+    }
 
-}
+    public int getRows(){
+        return rows;
+    }
+    
+    public int getColumns() {
+    	return columns;
+    }
+	
+    public static void main(String[] args) {
+    	
+    	Main mw = new Main();
 
+    	System.out.println(mw.rows);
+    	System.out.println(mw.columns);
+    }
+        
+}
 
