9b92a75ea931d62bfc7974763c152d8f7974bbef
LAPTOP-S14IBJ3C\rlpiq <rlpiquer@gmail.com>
2019-03-19 20:30:54 -0600

    anotherone
    anotherone

--- Tetris/src/Main.java
+++ Tetris/src/Main.java
@@ -5,6 +5,7 @@
  |_|   \__,_||_| |_| |_||_||_|   \___/ 
                                        
 */
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
@@ -13,15 +14,12 @@ public class Main {
 	private int rows;
 	private int columns;
 	private static int[][] boardgame;
-	private static int[] thelist;
 
 	// constructor with the values for row and columns
 	public Main() {
 		this.rows = 24;
 		this.columns = 10;
 		boardgame = new int[rows][columns];
-		 
-		thelist = new int[rows];
 		
 	}
 	// setters 
@@ -47,42 +45,58 @@ public class Main {
     public int getColumns() {
     	return columns;
     }
+    
+	// OP code that puts a row of 1's 
+    public static void rowsOfOne(){
+    	for (int r = 0; r < boardgame.length; r++) {
+    		for(int c = 0; c < boardgame[r].length; c++) {
+    			boardgame[2][c] = 1;
+    		}
+    	}
+    }
+    
+   	// prints out the 2d array board 
+    public static void printOutBoard() {
+    	for (int r = 0; r < boardgame.length; r++) {
+ 				System.out.println(Arrays.toString(boardgame[r]));		
+    	}
+    }
+    
+    public static void tracksTheRows() {
+    	for (int r = 0; r < boardgame.length; r++) {
+    	    System.out.println("start of row " + r);
+    	    for (int c = 0; c < boardgame[r].length; c++) {
+    	    	System.out.println(boardgame[r][c]);
+    	        // do stuff with row,col
+    	    }
+    	    // End of row
+    	}
+    	
+//    	for (...rows)
+//    	    boolean allOnes = true;
+//    	    for (...columns)
+//    	        if it's not a 1
+//    	            allOnes = false
+//    	    If allOnes
+//    	        doStuff
+//    
+    }
    
     // the place where it runs                            
     public static void main(String[] args) {
     	
-    	int[] thelist = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24};
-    	
     	// calls main
     	Main m = new Main();
-
-    	// OP code that puts a row of 1's 
-    	for (int r = 0; r < boardgame.length; r++) {
-    		for(int c = 0; c < boardgame[r].length; c++) {
-    			boardgame[10][c] = 1;
-    		}
-    	}
+    
+    	boardgame[0][1] = 1;
     	
-    	for (int i = 0; i < boardgame.length; i++) {
-    		for (int j = 0; j < boardgame[i].length; j++)
-    			if(boardgame[thelist[i] - 1][j] == 1) {
-    				boardgame[thelist[i] - 1][j] = 5;
-    			}
-    	}
-    	// prints out the 2d array board 
-    	for (int r = 0; r < boardgame.length; r++) {
- 				System.out.println(Arrays.toString(boardgame[r]));
-    			
-    	}
+    	rowsOfOne();
+    	tracksTheRows();
+    	printOutBoard();
 
     	// prints the values of the height and length in that order
     	System.out.println(m.rows);
     	System.out.println(m.columns);
-    	
-//    	for(int i = 0; i < thelist.length; i++) {
-//    		System.out.println(thelist[i]);
-//    	}
     
     	}
 }
-
